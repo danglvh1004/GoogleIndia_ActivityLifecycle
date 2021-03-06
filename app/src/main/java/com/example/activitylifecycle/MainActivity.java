6c23f98a0b2dc039af49774b04b8d42346cbde37
danglvh1004 <lvhd.hdlv@gmail.com>
2021-03-06 15:22:24 +0700

    Commit

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "onCreate Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "onStart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "onResume Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "onPause Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "onStop Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "onRestart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "onDestroy Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity", "onDestroy");
     }
 }
